39f30b0938320d39a34246455fe7df242dc72bee
121111631@qq.com <121111631@qq.com>
2018-03-09 10:06:23 +0800

    22
    222

--- src/main/java/wu/guo/App1.java
+++ src/main/java/wu/guo/App1.java
@@ -8,6 +8,7 @@ public class App1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello maven");
+		System.out.println("hello test1");
 	}
 
 }
