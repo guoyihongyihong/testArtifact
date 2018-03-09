ce31aebd684f41960dcd818935c3fab9eda5bcb6
121111631@qq.com <121111631@qq.com>
2018-03-09 10:03:32 +0800

    111
    1111

---
+++ src/main/java/wu/guo/App1.java
@@ -0,0 +1,13 @@
+package wu.guo;
+
+public class App1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello maven");
+	}
+
+}
